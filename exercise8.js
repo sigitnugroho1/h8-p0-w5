f3fe764eb68b55a6c001d59b14fb82465c827a48
sigit <sigitnugroho1>
2018-10-18 22:19:45 +0700

    week 5

---
+++ exercise8.js
@@ -0,0 +1,18 @@
+// Diberikan sebuah function cariPelaku(kalimat) yang menerima satu parameter berupa string. 
+// Function akan me-return jumlah berapa kali ditemukan kata "abc" secara berturut-turut di dalam kalimat tersebut. 
+// Gunakan regex untuk melatih kemampuan regex.
+
+
+function cariPelaku(str) {
+    // you can only write your code here!
+    var temp = str.match(/abc/g)
+    var result = temp.length 
+    return result
+  }
+  
+  // TEST CASES
+  console.log(cariPelaku('mabcvabc')); // 2
+  console.log(cariPelaku('abcdabdc')); // 1
+  console.log(cariPelaku('bcabcac')); // 1
+  console.log(cariPelaku('abcabcabc')); // 3
+  console.log(cariPelaku('babcbacabc')); // 2
